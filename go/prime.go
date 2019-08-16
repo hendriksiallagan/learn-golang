b7ba310604e4de0ae33621383b3f953f61bdb561
Hendrik Fanrio Siallagan <hendrik.fanrio@uangteman.com>
2019-08-16 16:56:20 +0700

    prime golang

---
+++ go/prime.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for  i:=1; i<=10; i++ {
+		 a := 0
+		
+		for  j:=1; j<=i; j++ {
+			if i%j==0 {
+				a++
+			}
+		}
+		
+		if a==2 {
+			fmt.Println(i)
+		}
+	
+	}
+	
+}
