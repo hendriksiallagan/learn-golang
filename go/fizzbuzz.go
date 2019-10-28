fd2f24cfbcda4dad49b68ad73713cf5b6040a99f
Hendrik <hendrik.fanrio@finaccel.co>
2019-10-28 17:26:34 +0700

    Fixed FizzBuzz

--- go/fizzbuzz.go
+++ go/fizzbuzz.go
@@ -2,19 +2,39 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
+
+/*
+	Create function from 1 to n parameter
+	If n can be divided by 3 and 5, return "Fizzbuzz"
+	If n can be divided by 3, return "Fizz"
+	if n can be divided by 5, return "Buzz"
+	else return n
+*/
+
+func FizzBuzz(n int) (res string) {
+
+	res = ""
+	for i:=1; i<=n; i++  {
+		if i%15 == 0 {
+			res += "FizzBuzz "
+		}  else if i%3 == 0 {
+			res+= "Fizz "
+		} else if i%5 == 0 {
+			res+="Buzz "
+		} else {
+			res += strconv.Itoa(i)
+			res += " "
+		} 	
+	}
+
+	return res
+}
 
 func main() {
 	
-	for i:=0; i<20; i++  {
-		if i%15 == 0 {
-		  fmt.Println("Fizz Buzz")
-		}  else if i%3 == 0 {
-	  	 fmt.Println("Fizz")
-		} else if i%5 == 0 {
-		  fmt.Println("Buzz")
-		} else {
-		 fmt.Println(i)
-		}
-	}
+	test_fb := FizzBuzz(15)
+
+	fmt.Println(test_fb)
 }
